1649180330b1c1f972450f1b3049bde4e111942a
llfbandit <llfbandit@gmail.com>
2024-07-06 01:22:25 +0200

    fix: Delay again plugin init because of binary messenger not initialized.

--- app_links_linux/CHANGELOG.md
+++ app_links_linux/CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.0.3
+* fix: Delay again plugin init because of binary messenger not initialized.
+
 ## 1.0.2
 * chore: Update dependency app_links_platform_interface to 2.0.0+ because of breaking changes.
 
--- app_links_linux/lib/app_links_linux.dart
+++ app_links_linux/lib/app_links_linux.dart
@@ -8,55 +8,80 @@ class AppLinksPluginLinux extends AppLinksPlatform {
     AppLinksPlatform.instance = AppLinksPluginLinux();
   }
 
-  late final StreamController<String> _controller;
-  late final GtkApplicationNotifier _notifier;
+  StreamController<String>? _controller;
+  GtkApplicationNotifier? _notifier;
   String? _initialLink;
   bool _initialLinkSent = false;
   String? _latestLink;
 
-  AppLinksPluginLinux() {
-    _controller = StreamController.broadcast()..onListen = _onListen;
+  // Initialize the plugin.
+  // This can't be done in the constructor because
+  // binary messenger hasn't been initialized at this stage.
+  void _init() {
+    _controller ??= StreamController.broadcast()..onListen = _onListen;
 
-    _notifier = GtkApplicationNotifier();
-    _notifier.addCommandLineListener((args) {
-      if (args.isNotEmpty) {
-        _send(args.first);
-      }
-    });
+    if (_notifier == null) {
+      _notifier = GtkApplicationNotifier();
+      _notifier?.addCommandLineListener((args) {
+        if (args.isNotEmpty) {
+          _send(args.first);
+        }
+      });
+    }
   }
 
   @override
   Future<Uri?> getInitialLink() async {
+    _init();
+
     if (_initialLink case final link?) {
-      return Uri.parse(link);
+      return Uri.tryParse(link);
     }
     return null;
   }
 
   @override
-  Future<String?> getInitialLinkString() async => _initialLink;
+  Future<String?> getInitialLinkString() async {
+    _init();
+
+    return _initialLink;
+  }
 
   @override
   Future<Uri?> getLatestLink() async {
+    _init();
+
     if (_latestLink case final link?) {
-      return Uri.parse(link);
+      return Uri.tryParse(link);
     }
     return null;
   }
 
   @override
-  Future<String?> getLatestLinkString() async => _latestLink;
+  Future<String?> getLatestLinkString() async {
+    _init();
+    return _latestLink;
+  }
 
   @override
-  Stream<String> get stringLinkStream => _controller.stream;
+  Stream<String> get stringLinkStream {
+    _init();
+    return _controller!.stream;
+  }
 
   @override
-  Stream<Uri> get uriLinkStream => _controller.stream.map(Uri.parse);
+  Stream<Uri> get uriLinkStream {
+    _init();
+
+    return _controller!.stream
+        .where((uri) => Uri.tryParse(uri) != null)
+        .map(Uri.parse);
+  }
 
   void _onListen() {
     if (!_initialLinkSent && _initialLink != null) {
       _initialLinkSent = true;
-      _controller.add(_initialLink!);
+      _controller?.add(_initialLink!);
     }
   }
 
@@ -65,9 +90,9 @@ class AppLinksPluginLinux extends AppLinksPlatform {
       _latestLink = uri;
       _initialLink ??= uri;
 
-      if (_controller.hasListener) {
+      if (_controller?.hasListener ?? false) {
         _initialLinkSent = true;
-        _controller.add(uri);
+        _controller?.add(uri);
       }
     }
   }
