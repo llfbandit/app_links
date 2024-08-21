faf0f524cda54223d290a1effba01ff6a1917ab3
llfbandit <llfbandit@gmail.com>
2024-08-21 18:31:17 +0200

    chore: Update Android example setup.

--- app_links/example/android/app/src/main/java/com/llfbandit/app_links_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.llfbandit.app_links_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- app_links/example/android/app/src/main/kotlin/com/llfbandit/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.llfbandit.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
